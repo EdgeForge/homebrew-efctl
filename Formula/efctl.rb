e88107cc5b6b94b926004c21318b17ea62dd8b75
magicalyak <tom.gamull@gmail.com>
2022-08-12 17:02:50 -0400

    tttt

--- Formula/efctl.rb
+++ Formula/efctl.rb
@@ -7,6 +7,7 @@ class Efctl < Formula
     url "https://github.com/EdgeForge/efctl/archive/refs/tags/v0.0.3.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "87f51fdd64c3e8895ec4dd7c09d06f53dc9b3d36fe5d3b4bb9f62805b990903d"
     head "https://github.com/EdgeForge/efctl.git"
+    version 0.0.3
 
     depends_on "node" => :build
 
