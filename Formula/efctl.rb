5bdb0fcd100b52ec68e3925fe18db65b59f0d838
magicalyak <tom.gamull@gmail.com>
2022-08-12 17:00:25 -0400

    rr

--- Formula/efctl.rb
+++ Formula/efctl.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Efctl < Formula
     desc "CLI to use EdgeForge"
     homepage "https://github.com/EdgeForge/efctl"
-    url "https://github.com/EdgeForge/efctl/archive/refs/tags/v0.0.3.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+    url "https://github.com/EdgeForge/efctl/archive/refs/tags/v0.0.3.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "87f51fdd64c3e8895ec4dd7c09d06f53dc9b3d36fe5d3b4bb9f62805b990903d"
     head "https://github.com/EdgeForge/efctl.git"
 
